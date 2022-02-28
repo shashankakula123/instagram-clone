0eaf6a0b30dd0164d2716aab06ca5f523808c2c1
shashankakula123[A[Agit <shashank310oro@gmail.com>
2022-02-28 13:51:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+<div className='app__header'>
+  <img className='app__headerImage' src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png' alt='instagram' />
+
+</div>
+
+
+    </div>
+  );
+}
+
+export default App;
